5cc36c0e7aae56025e3fb188e68648232ee055e5
JahadB <beylercahad@gmail.com>
2025-02-20 14:19:19 +0300

    Added ticket generation(v1)

--- conference_ticket_generator/script.js
+++ conference_ticket_generator/script.js
@@ -1,7 +1,33 @@
 const imgUpload = document.getElementById("avatar");
+const dropZone = document.querySelector(".dropZoneOverlay");
+const form = document.getElementsByTagName("form")[0];
+const submitBtn = document.getElementById("submitBtn");
+const ticket = document.querySelector(".ticket");
 
 imgUpload.addEventListener("change", Upload);
 
 function Upload() {
-    imgUpload.files[0]
+    let imgURL = URL.createObjectURL(imgUpload.files[0]);
+    dropZone.style.backgroundImage = `url(${imgURL})`;
+    dropZone.textContent = "";
+    dropZone.style.backgroundPosition = "center";
+    dropZone.style.backgroundSize = "90px 90px";
+    dropZone.style.backgroundRepeat = "no-repeat";
+}
+
+function getData(form) {
+    var formData = new FormData(form);
+
+    for (const [key, value] of formData) {
+        comsole.log(value);    
+    }
+  
+}
+form.addEventListener("submit", generateTicket);
+
+function generateTicket(event) {
+    event.preventDefault();
+    form.style.display = "none";
+    ticket.style.display = "flex";
+    getData(event.target);
 }
